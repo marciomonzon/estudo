fed01daab02999c125deae67919fbc8732cd07b7
Marcio <marcio.monzon@smsolucao.com.br>
2019-07-06 17:24:41 -0300

    Adicionar arquivos de projeto.

---
+++ EscrevendoCodigoEmExecuxao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscrevendoCodigoEmExecuxao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = typeof(int);
+            Func<int, int, int> soma = Soma; // delegate
+
+            var dynamicMethod = new DynamicMethod("SomaCriadoEmExecucao", type, new[] { type, type });
+            var ilGenerator = dynamicMethod.GetILGenerator();
+
+            //ilGenerator.Emit(OpCode,Ldarg_0);
+            //ilGenerator.Emit();
+            //ilGenerator.Emit();
+            //ilGenerator.Emit();
+        }
+
+        static int Soma(int valorA, int valorB)
+        {
+            return valorA + valorB;
+        }
+    }
+}
---
+++ estudo.dynamicsvar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudo.dynamicsvar
+{
+    class Program
+    {
+        // var is statically typed
+        // dynamic is late bounded or checked on runtime
+
+        static void Main(string[] args)
+        {
+            var x1 = "string"; // earlu bounded
+            int len = x1.Length;
+
+            dynamic y = "string1"; // late bounded
+            int len1 = y.Length;
+        }
+    }
+}

1194e274c20014abb13c4cb7886a799dabc1b9b5
Marcio <marcio.monzon@smsolucao.com.br>
2019-07-06 19:55:56 -0300

    exemplo de linq

---
+++ IEnmurableDissecado/MaquinaEstado.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnmurableDissecado
+{
+    public class MaquinaEstado : IEnumerator<int>, IEnumerator
+    {
+        private int estado;
+
+        public MaquinaEstado()
+        {
+            estado = 0;
+        }
+
+        public int Current => throw new NotImplementedException();
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public bool MoveNext()
+        //{
+        //    switch (estado)
+        //    {
+        //        case 0: Current = 1; estado = 1; return true;
+        //        default:
+        //            break;
+        //    }
+        //}
+
+        public void Reset()
+        {
+            throw new NotSupportedException("Cire uma nova instância");
+        }
+    }
+}
